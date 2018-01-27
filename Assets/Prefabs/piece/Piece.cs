1c4830dab95ad28db398f30c46379a38dc6eb0b0
Joe Cabezas Campos <joe.cabezas@gmail.com>
2018-01-26 23:29:32 -0800

    snapping first iteration

---
+++ Assets/Prefabs/piece/Piece.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+
+    public Collider[] snapPoints;
+    public bool isBeingManipulated;
+
+    public List<Piece> potentialChildList;
+
+	// Use this for initialization
+	void Start ()
+    {
+        snapPoints = GetComponentsInChildren<Collider>();
+	}
+
+    public void addPotentialPiece(Piece piece)
+    {
+        if (isBeingManipulated) return;
+        Debug.Log("Piece: Adding piece:" + piece);
+        potentialChildList.Add(piece);
+    }
+
+    public void removePotentialPiece(Piece piece)
+    {
+        if (isBeingManipulated) return;
+        Debug.Log("Piece: Removing piece:" + piece);
+        potentialChildList.Remove(piece);
+    }
+}
---
+++ Assets/Prefabs/piece/SnapPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapPoint : MonoBehaviour {
+
+    public Piece parentPiece;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Piece piece = getPieceInCollision(other);
+        parentPiece.addPotentialPiece(piece);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Piece piece = getPieceInCollision(other);
+        parentPiece.removePotentialPiece(piece);
+    }
+
+    private Piece getPieceInCollision(Collider other)
+    {
+        Piece piece = other.GetComponentInParent<Piece>();
+        if (piece)
+        {
+            return piece;
+        }
+        return null;
+    }
+}
