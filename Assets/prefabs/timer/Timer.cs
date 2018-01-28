6cd1a038c2dcc6eaa53a92d499bd290215876b54
Alec Von Arx <alecva@amazon.com>
2018-01-28 15:00:43 -0800

    Add simple timer to UI

---
+++ Assets/prefabs/timer/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Author: Frj
+public class Timer : MonoBehaviour {
+
+    public Text timeText;
+
+    private float time;
+
+    // Use this for initialization
+    void Start() {
+        time = 0.0F;
+        updateTimeText();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        time += Time.deltaTime;
+        updateTimeText();
+    }
+
+    void updateTimeText() {
+        // Round time to nearest hundredth
+        timeText.text = Math.Round(time, 2).ToString();
+    }
+}
